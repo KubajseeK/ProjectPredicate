4163bba24d22b4ba7a259f729544e371147e58b7
Jakub Kutka <jacobkutka@gmail.com>
2021-02-03 18:38:50 +0100

    JSON parsing done.

--- src/sk/itsovy/kutka/Main.java
+++ src/sk/itsovy/kutka/Main.java
@@ -1,10 +1,14 @@
 package sk.itsovy.kutka;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
+import org.json.*;
+import com.google.gson.*;
 
 public class Main {
 
@@ -14,8 +18,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         URL url = new URL(dataUrl);
+        JsonParser jsonParser = new JsonParser();
+
         URLConnection urlConnection = url.openConnection();
         urlConnection.connect();
+
+        /*
+         * https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java/4308662
+         */
+
+        JsonElement rootElement = jsonParser.parse(new InputStreamReader((InputStream) urlConnection.getContent()));
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonArray worldXArray = rootObject.get("world_x").getAsJsonArray();
+
+        System.out.println(worldXArray.get(1));
+
 
 
     }
